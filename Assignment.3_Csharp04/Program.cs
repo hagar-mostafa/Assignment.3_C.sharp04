31bbe2e94e6bdd2af36662fb90845d90c8b3e373
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-05 20:04:58 +0200

    Solve Q1

--- Assignment.3_Csharp04/Program.cs
+++ Assignment.3_Csharp04/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 # nullable disable
 internal class Program
 {
+        
     static void Main(string[] args)
     {
-       
+        #region Question 1
+        double[] prices = new double[] { 25.5, 40.0, 33.75 };
+        Console.WriteLine($"The second element of the array is: {prices[1]}");
+        #endregion
+
+        Console.WriteLine("----------------------------------------------");
+
+        #region Question 2
+        int[,] shelfCopies = new int[,] {
+        {3 ,5 }, // shelf 0
+        { 2 , 6 }, // shelf 1
+        { 1 , 4}
+        };
+        Console.WriteLine($"The number of copies on shelf 1, slot 0: {shelfCopies[1, 0]}");
+        #endregion
+
+        Console.WriteLine("----------------------------------------------");
+
+        #region Question 3
+        void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Library!");
+        }
+        PrintWelcomeMessage();
+        #endregion
+
+        Console.WriteLine("----------------------------------------------");
 
 
     }
